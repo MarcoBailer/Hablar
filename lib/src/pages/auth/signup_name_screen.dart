32b0af92724169cc38b4d4f5ecf2078afe968f33
Marco Willian <maicolawgames@gmail.com>
2024-10-09 20:28:22 -0300

    ip

--- lib/src/pages/auth/signup_name_screen.dart
+++ lib/src/pages/auth/signup_name_screen.dart
@@ -293,7 +293,7 @@ class _SignupNameScreenState extends State<SignupNameScreen> {
 
     final client = createIOClient();
 
-    final url = Uri.parse('https://172.16.59.23:7235/api/Auth/Register');
+    final url = Uri.parse('https://{ip-da-maquina}:7235/api/Auth/Register');
     final headers = {'Content-Type': 'application/json'};
     final body = jsonEncode({
       'firstName': firstName,
